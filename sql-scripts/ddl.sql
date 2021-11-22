0b817fd59051d266cab6ea898c0fd30cbea5ea48
Johan Kristoffersson <joki20@student.bth.se>
2021-11-22 19:18:08 +0000

    reverserat ändringar, check av närheten av station måste ske i frontend

--- sql-scripts/ddl.sql
+++ sql-scripts/ddl.sql
@@ -143,7 +143,7 @@ CREATE FUNCTION calc_geo_dist(
     end_lat DECIMAL(9, 6),
     end_lon DECIMAL(9, 6)
 )
-RETURNS DECIMAL(9, 6)
+RETURNS INT
 DETERMINISTIC
 BEGIN
 	SET @delta_lat = end_lat - start_lat;
@@ -188,7 +188,6 @@ BEGIN
         NOT OLD.customer_id IS NULL AND
         NEW.customer_id IS NULL
         ) THEN
-        SET @scooter_id = (SELECT id FROM scooter WHERE NOT OLD.customer_id IS NULL AND NEW.customer_id IS NULL);
         -- start cost
         SET @start_cost = 20;
         -- travelling cost
@@ -239,18 +238,7 @@ BEGIN
         -- TOTAL COST FOR SINGLE SCOOTER TRIP
         --
 
-        -- station id if ended within station
-        SET @station_id_if_at_station = 0;
-        IF @ended_at_station = 1 THEN
-            SET @station_id_if_at_station = (
-                SELECT id FROM station
-                WHERE calc_geo_dist(@start_lat, @start_lon, lat_center, lon_center) <= radius
-            );
-        END IF;
-
         -- if scooter was collected outside allowed zone and parked near charging station or zone, give discount
-        IF $ended_inside_city = 1 THEN
-
         IF @started_inside_city = 0 AND @ended_inside_city = 1 THEN
             SET @start_cost = @start_cost - @bring_home_discount;
         END IF;
@@ -258,15 +246,6 @@ BEGIN
         -- if ending at a station, add normal parking cost
         IF @ended_at_station = 1 THEN
             SET @total_cost = @start_cost + @travel_cost + @parking_cost;
-
-            -- position scooter at this station
-            UPDATE scooter
-            SET
-                station_id = @station_id_if_at_station
-            WHERE
-                id = @scooter_id;
-		END IF;
-
         -- if ending in city but not at a station, add city cost
         ELSEIF @ended_inside_city = 1 THEN
             SET @parking_cost = @parking_cost + 20;
